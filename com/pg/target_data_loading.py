4a0100c36c51e6e8789ebb21429973b8c2ce59ce
asinha712 <asinha712@gmail.com>
2020-10-18 06:38:24 +0100

    sasd

---
+++ com/pg/target_data_loading.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+import yaml
+import os.path
+import com.pg.utils.utility as utils
+import pyspark.sql.functions as f
+
+
+if __name__ == '__main__':
+
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
+    )
+
+
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .master('local[*]') \
+        #.config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+# Setup spark to use s3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
+    tgt_list = app_conf["target_list"]
+    for src in tgt_list:
+        src_conf = app_conf[src]
+        if src == 'REGIS_DIM':
+            one_cp_df = spark.read \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/staging_pg/"+src_conf["source_data"]) \
+                .repartition(5)
+            one_cp_df.show()
